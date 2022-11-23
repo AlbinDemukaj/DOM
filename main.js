5a9b35d1b2e60b94af03f6790d80bb8f0e8373b4
AlbinDemukaj <albin.demukaj@student.uni-pr.edu>
2022-11-23 20:59:13 +0100

    commits

---
+++ main.js
@@ -0,0 +1,51 @@
+// d1
+// var playList = [
+//     {
+//      author: "LED ZEPPELIN",
+//      song:"STAIRWAY TO HEAVEN"
+//     },
+//     {
+//      author: "QUEEN",
+//      song:"BOHEMIAN RHAPSODY"
+//     },
+//     {
+//      author: "LYNYRD SKYNYRD",
+//      song:"FREE BIRD"
+//     },
+//     {
+//      author: "DEEP PURPLE",
+//      song:"SMOKE ON THE WATER"
+//     },
+//     {
+//      author: "JIMI HENDRIX",
+//      song:"ALL ALONG THE WATCHTOWER"
+//     },
+//     {
+// author: "AC/DC",
+// song:"BACK IN BLACK"
+// },
+//     {
+//      author: "QUEEN",
+//      song:"WE WILL ROCK YOU"
+// },
+//     {
+//      author: "METALLICA",
+//      song:"ENTER SANDMAN"
+//     }
+//     ];
+// let songs=document.querySelectorAll('li');
+// for(let i=0;i<songs.length;i++){
+// songs[i].innerHTML=`Author: ${playList[i].author} -- Song: ${playList[i].song}`;
+// }
+
+
+//d2
+// let open=document.getElementById('openBTN');
+// let close=document.getElementById('closeBTN');
+// let modalBox=document.getElementById('modal-box');
+// open.onclick = function(){
+//     modalBox.style.display = 'block';
+// }
+// close.onclick = function(){
+//     modalBox.style.display = 'none';
+// }
